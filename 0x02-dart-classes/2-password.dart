07bcf40a6df60e516d8eced03ab4b5d679237efb
kkeas <keaskatrina@gmail.com>
2024-06-18 15:24:10 -0500

    1 should work but main file for 2 was weird

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,32 @@
+class Password {
+  String password;
+
+  Password(this.password);
+
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+
+    bool hasUppercase = false;
+    bool hasLowercase = false;
+    bool hasNumber = false;
+
+    for (var char in password.split('')) {
+      if (char.contains(RegExp(r'[A-Z]'))) {
+        hasUppercase = true;
+      } else if (char.contains(RegExp(r'[a-z]'))) {
+        hasLowercase = true;
+      } else if (char.contains(RegExp(r'[0-9]'))) {
+        hasNumber = true;
+      }
+    }
+
+    return hasUppercase && hasLowercase && hasNumber;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
