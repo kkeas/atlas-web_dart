b4ea4742d00f329f98d4be3f1779eee96e1ac5cc
kkeas <keaskatrina@gmail.com>
2024-06-24 12:17:53 -0500

    task 2

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,14 @@
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    String userData = await fetchUser();
+    print('$userData');
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
+
+void main() async {
+  await getUser();
+}
